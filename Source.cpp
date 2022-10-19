ab1cd4893f0e1a29a08ea94e3e0bed14e933e997
MagdElDinAhmed <magdeldin@aucegypt.edu>
2022-10-19 21:45:19 +0200

    fixed inputs and added validation

--- Source.cpp
+++ Source.cpp
@@ -3,95 +3,33 @@
 #include "implicant.h"
 #include <fstream>
 #include <cctype>
+#include<math.h>
+#include <set>
+//#include "InputLib.h"
 using namespace std;
+void Extract(vector<int>& fucntion_minterms, vector<int>& fucntion_dontcares, int& no_of_variables, bool& valid_in);
+bool CheckValidVarSize(string in, int no_of_variables);
+bool CheckValidTerm(string in, int no_of_variables, set<int>& terms);
+bool CheckRangeVarCount(string in, int size);
+bool CheckRangeTerm(string in, int size);
+bool CheckCharacter(string in);
+
+
+
+
+
+
 int main()
 {
 	vector<int> function_minterms;
 	vector<int> function_dontcares;
 	vector <implicant> prime_implicants;
 	int no_of_variables=-1;
-	ifstream file;
-	file.open("input.txt");
-	char temp;
-	string temp2;
+
 	bool valid_in = true;
 
-	//this bulk of code extracts the number of variables
 
-	file.get(temp);
-	if (isdigit(temp))
-	{
-		if (isdigit(file.peek()))
-		{
-			temp2 = temp;
-			file.get(temp);
-			if (isdigit(file.peek())) //checks if number has 3 digits
-			{
-				valid_in = false;
-			}
-			temp2 += temp;
-			no_of_variables= stoi(temp2);
-		}
-		else {
-			no_of_variables = (int)(temp - '0');
-		}
-		
-	}
-	if (no_of_variables < 1 || no_of_variables>20)
-	{
-		valid_in = false;
-	}
-	file.get(temp);//disposing of the '\n'
-
-	//this do while loop extracts the minterms
-	do
-	{
-		file.get(temp);
-		if (isdigit(temp))
-		{
-			if (isdigit(file.peek()))
-			{
-				temp2 = temp;
-				file.get(temp);
-				temp2 += temp;
-				function_minterms.push_back(stoi(temp2));
-			}
-			else {
-				function_minterms.push_back((int)(temp - '0'));
-			}
-		}
-		else if (isalpha(temp) || (ispunct(temp) && temp != ','))
-		{
-			valid_in = false;
-		}
-	} while (file.peek() != '\n' && valid_in);
-
-	file.get(temp);//disposing of the '\n'
-
-	//this piece of code extracts the don't care terms
-	do
-	{
-		file.get(temp);
-		if (isdigit(temp) && temp!=EOF)
-		{
-			if (isdigit(file.peek()))
-			{
-				temp2 = temp;
-				file.get(temp);
-				temp2 += temp;
-				function_dontcares.push_back(stoi(temp2));
-			}
-			else {
-				function_dontcares.push_back((int)(temp - '0'));
-			}
-		}
-		else if (isalpha(temp) || (ispunct(temp) && temp != ','))
-		{
-			valid_in = false;
-		}
-	} while (file.peek() != EOF && valid_in);
-
-
+	Extract(function_minterms, function_dontcares, no_of_variables, valid_in);
 
 	if (valid_in)
 	{
@@ -115,7 +53,120 @@ int main()
 
 }
 
+void Extract(vector<int>& fucntion_minterms, vector<int>& fucntion_dontcares, int& no_of_variables, bool& valid_in)
+{
+	ifstream file;
+	string temp, temp2;
+	char new_line_trash;
+	bool new_line=false;
+	set<int>terms;
+	file.open("input.txt");
+	getline(file, temp);
+	if (!CheckValidVarSize(temp, 20))
+	{
+		valid_in = false;
+		return;
+	}
+	no_of_variables = stoi(temp);
+	while (!new_line && valid_in)
+	{
+		getline(file, temp, ','); //check for new line
+		for (int i = 0; i < temp.length(); i++)
+		{
+			if (temp[i] == '\n')
+			{
+				temp2 = temp.substr(i+1, string::npos);
+				temp = temp.substr(0, temp.length() - (temp.length() - i));
+				new_line = true;
+			}
 
+		}
+		valid_in = CheckValidTerm(temp, no_of_variables, terms);
+		if (valid_in)
+		{
+			fucntion_minterms.push_back(stoi(temp));
+		}
+	}
+	if (valid_in)
+	{
+		valid_in = CheckValidTerm(temp2, no_of_variables, terms);
+		if (valid_in)
+		{
+			fucntion_dontcares.push_back(stoi(temp2));
+		}
+	}
+	
+
+	while (!file.eof() && valid_in)
+	{
+		getline(file, temp, ',');
+		valid_in = CheckValidTerm(temp, no_of_variables, terms);
+		if (valid_in)
+		{
+			fucntion_dontcares.push_back(stoi(temp));
+		}
+	}
+}
+
+bool CheckValidVarSize(string in, int no_of_variables)
+{
+	bool valid = true;
+	valid = (CheckRangeVarCount(in, 20) && CheckCharacter(in));
+	return valid;
+}
+bool CheckValidTerm(string in, int no_of_variables, set<int>& terms)
+{
+	bool valid = true;
+	int size = (pow(2, no_of_variables) - 1);
+	valid = (CheckRangeTerm(in, size) && CheckCharacter(in) && (terms.find(stoi(in)) == terms.end()));
+	if (valid)
+	{
+		terms.insert(stoi(in));
+	}
+	return valid;
+}
+bool CheckRangeVarCount(string in, int size)
+{
+	bool valid;
+	int tempNum = stoi(in);
+	if (tempNum > 0 && tempNum <= size)
+	{
+		valid = true;
+	}
+	else
+	{
+		valid = false;
+	}
+	return valid;
+}
+bool CheckRangeTerm(string in, int size)
+{
+	bool valid;
+	int tempNum = stoi(in);
+	if (tempNum >= 0 && tempNum <= size)
+	{
+		valid = true;
+	}
+	else
+	{
+		valid = false;
+	}
+	return valid;
+}
+bool CheckCharacter(string in)
+{
+	bool valid = true;
+	int i = 0;
+	while (i < in.length() && valid)
+	{
+		if (isalpha(in[i]) || ispunct(in[i]))
+		{
+			valid = false;
+		}
+		i++;
+	}
+	return valid;
+}
 
 /*
 * So the plan is to operate on 20 bits
